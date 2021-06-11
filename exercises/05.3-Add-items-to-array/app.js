e11b6c8d79c6a1b0e5fee8fc181a03869c4bccd4
kevincastro015 <kevin.castro015@mymdc.net>
2021-06-11 21:17:39 +0000

    5.3AddItemsToArray

--- exercises/05.3-Add-items-to-array/app.js
+++ exercises/05.3-Add-items-to-array/app.js
@@ -1,5 +1,7 @@
-var arr = [4,5,734,43,45];
-
+var arr = [4,5,734,43,45,2,4];
 // Your code here, use the push function and the random function.
-
+function randomNumber() {
+    var num = Math.floor((Math.random() * 7) + 1);
+    arr.push(num);
+}    
 console.log(arr);
--- exercises/05.4-Add-items-to-array-looping/app.js
+++ exercises/05.4-Add-items-to-array-looping/app.js
@@ -1,10 +1,18 @@
-var arr = [4,5,734,43,45];
+var arr = [4,5,734,43,45,9,3,5,4,1,9,23,4,56,78];
 
 //*****************
 // Your code here
 // you need to loop 10 times, for example, using a for loop
 // for(let i = 0; i<10;i++){
-//         your loop content here   
+//         your loop content here
+for (var i=0;i<=15; i++) {
+      value10 = value * 10;
+      document.write(`${value} multiply 10 -> ${value10}`);
+      document.write('<br>');
+      //i placed this if statement to break out of the for loop as it //will run forever
+      value++;
+
+    }   
 // }
 //*****************
 
