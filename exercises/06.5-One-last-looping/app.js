6d0144849bf45f29650ece73e9d55378f7d447d9
kevincastro015 <kevin.castro015@mymdc.net>
2021-06-16 12:28:56 +0000

    6.5OneLastLooping

--- exercises/06.5-One-last-looping/app.js
+++ exercises/06.5-One-last-looping/app.js
@@ -1,3 +1,9 @@
 let mySampleArray = ['Esmeralda', 'Kiko', 'Ruth', 'Lebron', 'Pedro', 'Maria', 'Lou', 'Fernando', 'Cesco', 'Bart', 'Annie'];
 
 //your code here
+let nuevo =[]; //array auxiliar
+ for(let i = mySampleArray.length-1; i >=0; i--) {
+ console.log(mySampleArray[i])// aqui te imprime lo eslementoe de forma inversa
+ nuevo.push(mySampleArray[i]); // creas un nuevo arrray
+ }
+//  console.log(nuevo)// cuando lo muestras ya tienes un array con los elementos de forma inversa
