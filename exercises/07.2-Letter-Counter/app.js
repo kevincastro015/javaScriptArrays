72b617026829fc951c3d6e83651e1940e7d4aa28
kevincastro015 <kevin.castro015@mymdc.net>
2021-06-22 00:33:39 +0000

    7.2LetterCounter

--- exercises/07.2-Letter-Counter/app.js
+++ exercises/07.2-Letter-Counter/app.js
@@ -1,6 +1,15 @@
-let par = "Lorem ipsum dolor sit amet consectetur adipiscing elit Curabitur eget bibendum turpis Curabitur scelerisque eros ultricies venenatis mi at tempor nisl Integer tincidunt accumsan cursus"
-let counts = {};
-
-// your code here
-
+let par = "Lorem ipsum dolor sit amet consectetur adipiscing elit Curabitur eget bibendum turpis Curabitur scelerisque eros ultricies venenatis mi at tempor nisl Integer tincidunt accumsan cursus"
+let counts = {};
+par = par.toLowerCase();
+// your code here
+for (let i = 0; i < par.length; i++) {
+    if (par[i] === " ") {
+        continue
+    }
+    if (!counts[par[i]]) {
+        counts[par[i]] = 1
+    } else {
+        counts[par[i]]++
+    }
+}
 console.log(counts);
